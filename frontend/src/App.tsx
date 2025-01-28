994191e30ef1909161ed5b67906cea28a556463e
thuyiya <thusitha.4t@gmail.com>
2025-01-28 21:55:24 +0530

    ProtectedRoutes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,17 +6,20 @@ import UserManagement from './pages/dashboard/user-mangement';
 import Settings from './pages/dashboard/settings';
 import IdeaManagement from './pages/dashboard/idea-management';
 import LoginPage from './pages/auth/login';
+import ProtectedRoutes from './utils/ProtectedRoutes';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<Dashboard />} />
-          <Route path="/user" element={<UserManagement />} />
-          <Route path="/settings" element={<Settings />} />
-          <Route path="/idea" element={<IdeaManagement />} />
+        <Route path="/" element={<ProtectedRoutes />}>
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="/user" element={<UserManagement />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/idea" element={<IdeaManagement />} />
+          </Route>
         </Route>
       </Routes>
     </Router>
