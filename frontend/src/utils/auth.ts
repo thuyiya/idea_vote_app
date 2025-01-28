871e5901d9880e96e7297a7516b5f42b044d1a0a
thuyiya <thusitha.4t@gmail.com>
2025-01-28 19:34:07 +0530

    remove

--- frontend/src/utils/auth.ts
+++
@@ -1,9 +0,0 @@
-export const isAuthenticated = () => {
-    return !!localStorage.getItem('token'); // Check if a token exists
-};
-
-export const logout = () => {
-    localStorage.removeItem('token'); // Remove the token
-    window.location.href = '/login'; // Redirect to the login page
-};
-
